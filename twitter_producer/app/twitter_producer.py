6c2d60059cb7d0ce4cfb53fb9dc7fe4fc1a2fbfb
Casey Frost <casey.frost@alumni.usc.edu>
2022-09-07 07:34:08 -0700

    Update twitter_producer.py

--- twitter_producer/app/twitter_producer.py
+++ twitter_producer/app/twitter_producer.py
@@ -1,3 +1,5 @@
+"""Uses the Twitter API V2 to create a stream of tweets about SFBART which his then sent to a kafka topic"""
+
 import json
 from tweepy import StreamingClient, StreamRule
 from kafka import KafkaProducer
@@ -9,13 +11,13 @@ class TweetPrinter(StreamingClient):
     def __init__(self, bearer_token, **kwargs):
         super().__init__(bearer_token, **kwargs)
         self.bearer_token = bearer_token_import
-        self.kafka_topic = "BART_TWEETS"
-        self.kafka_producer = KafkaProducer(bootstrap_servers=['kafka:9092'], api_version=(0, 11, 5),
-                                            value_serializer=lambda v: json.dumps(v).encode('utf-8'),
-                                            key_serializer=str.encode)
+        self.kafka_topic = "BART_TWEETS"  # kafka topic needed in ksql script
+        self.kafka_producer = KafkaProducer(bootstrap_servers=['kafka:9092'], api_version=(0, 11, 5),  # kafka container port 'kafka'
+                                            value_serializer=lambda v: json.dumps(v).encode('utf-8'),  # message is sent in json
+                                            key_serializer=str.encode)  # the key is a string
 
     def on_tweet(self, tweet):
-        sleep(60)
+        sleep(120)  # trying not to reach the rate limit on the Twitter API
         message = {
                 "TWEET_ID": tweet.id,
                 "CREATED_DATE": tweet.created_at,
@@ -25,6 +27,7 @@ class TweetPrinter(StreamingClient):
         self.kafka_producer.flush()
 
     def create_rules(self):
+        # rules to query tweets
         rule_ids = []
         result = self.get_rules()
         for rule in result.data:
